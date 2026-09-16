a71f711203156334aa8f3e77252b6dd6eee391a2
anyeligalvamota-hash <anyeligalvamota@gmail.com>
2026-09-17 01:18:08 +0200

    Actualizar caché del service worker para las mejoras de Finanza

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='finanza-v36';
+const CACHE='finanza-v37';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/manifest.webmanifest','/icon.svg'])))});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
